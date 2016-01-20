946b890a28a8c3f6dcba27ac1331b118bf564181
Wesley de Groot <wes@vista.aero>
2016-01-20 16:26:07 +0100

    Fixed pre-commits

--- _pre_commit.sh
+++ _pre_commit.sh
@@ -4,16 +4,18 @@ if [ -e ".git/hooks/pre-commit" ]; then
 	VER=`git log --oneline | wc -l`
 	TAGVER=`expr $VER + 1`
 	MYVERS=`cat _ver`
-	TAGVER=`$MYVERS.$TAGVER`
+	TAGVER=`echo $MYVERS.$TAGVER`
 
 	#Need to update Xcode version for framework...
 	cd PHPFramework
-	xcrun agvtool new-version -all TAGVER
+	xcrun agvtool new-version -all $TAGVER &> /dev/null
 	# ok
 else
 	echo 'Creating pre-commit git hook...'
 	echo '#!/bin/sh' > .git/hooks/pre-commit
-	echo 'if [ -x ./_pre-commit.sh ]; then' >> .git/hooks/pre-commit
-	echo '	source ./_pre-commit.sh' >> .git/hooks/pre-commit
+	echo 'if [ -x ./_pre_commit.sh ]; then' >> .git/hooks/pre-commit
+	echo '	source ./_pre_commit.sh' >> .git/hooks/pre-commit
+	echo 'else' >> .git/hooks/pre-commit
+	echo '	echo MISSING PRE-COMMIT' >> .git/hooks/pre-commit
 	echo 'fi' >> .git/hooks/pre-commit
 fi
